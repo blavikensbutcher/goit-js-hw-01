cb6190f8db2649c989ba3ed5a6210055574f2e22
blavikensbutcher <v.pidnebesnyi@omisoft.net>
2025-08-20 20:25:31 +0300

    done

---
+++ js/task-2.ts
@@ -0,0 +1,24 @@
+const getShippingMessage = (
+  country: string,
+  price: number,
+  deliveryFee: number
+): string => {
+  if (!country || country.trim().length <= 1) {
+    throw new Error("Invalid country name");
+  }
+
+  if (price <= 0) {
+    throw new Error("Price must be greater than 0");
+  }
+
+  if (deliveryFee <= 0) {
+    throw new Error("Delivery fee must be greater than 0");
+  }
+
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
+};
+
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
---
+++ js/task-3.ts
@@ -0,0 +1,18 @@
+const getElementWidth = (content: string, padding: string, border: string) => {
+  if (
+    !content.includes("px") ||
+    !padding.includes("px") ||
+    !border.includes("px")
+  ) {
+    throw new Error("That should be pixels");
+  }
+
+  const resultWidth =
+    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+
+  return resultWidth;
+};
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
